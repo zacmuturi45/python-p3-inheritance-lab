a79d0fd74685c6747c6f1760425ca3a0fd5d3722
isaacgichuhi <zacmuturi45@gmail.com>
2023-12-01 18:05:56 +0300

    Completed assignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, string):
+        self.string = string
+        return self.knowledge.append(self.string)
+        
